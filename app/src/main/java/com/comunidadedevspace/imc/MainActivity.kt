e699a3d34c36bd0932f285a70e09f33a7a352dd5
Revplay1 <marco.passos@icloud.com>
2024-11-24 10:56:18 +0100

    IMC edit text

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,28 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //values for components inputText
+
+        val itextweight = findViewById<TextInputEditText>(R.id.weightInput)
+        val itextheight = findViewById<TextInputEditText>(R.id.heightInput)
+
+        // value for result button
+        val imcresult = findViewById<Button>(R.id.imcButton)
+
+        // Button click action
+        imcresult.setOnClickListener {
+
+
+        }
+
+
     }
 }
